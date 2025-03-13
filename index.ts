34e2d4fceb65ce4d935250d7ca67e885eb82fd21
Luiz Cieslak <cieslakluiz@gmail.com>
2025-03-13 11:57:07 -0300

    lint

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-console.log("Hello via Bun!");
+console.log('Hello via Bun!')
 
 const server = Bun.serve({
-  port: 5634,
-  fetch(req) {
-    return new Response("Bun!");
-  },
-});
+	port: 5634,
+	fetch(req) {
+		return new Response('Bun!')
+	},
+})
 
-console.log(`Listening on http://localhost:${server.port} ...`);
+console.log(`Listening on http://localhost:${server.port} ...`)
